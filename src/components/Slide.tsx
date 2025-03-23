18913745c58e5d86cd27852163c47e31f2b99320
Kristian Ionescu <ionescu.kristian@gmail.com>
2025-03-23 17:58:51 +0100

    layout fix

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -8,9 +8,7 @@ type SlideProps = {
 
 export const Slide = ({ children, className = "" }: SlideProps) => {
   return (
-    <section
-      className={`h-screen w-full snap-start flex items-center justify-center bg-zinc-100 dark:bg-zinc-900 ${className}`}
-    >
+    <section className="h-screen w-full snap-start flex md:items-center items-start justify-center pt-6 md:pt-0 bg-zinc-100 dark:bg-zinc-900 ${className}">
       <div className="max-w-3xl w-[90%] h-[80%] bg-white dark:bg-zinc-800 rounded-2xl shadow-2xl p-6">
         {children}
       </div>
